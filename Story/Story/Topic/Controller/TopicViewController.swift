11ff036179c7c93db25df072e1f553bda2893387
Aries <37924647+ariesdwi@users.noreply.github.com>
2020-06-12 03:30:59 +0700

    Prepare CardView
    Menambahkan cardview pada storyboard

--- Story/Story/Topic/Controller/TopicViewController.swift
+++ Story/Story/Topic/Controller/TopicViewController.swift
@@ -8,23 +8,30 @@
 
 import UIKit
 
-class TopicViewController: UIViewController {
+class TopicViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+       
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
     }
-    */
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "topic", for: indexPath) as? topicCollectionViewCell else {
+            // we failed to get a PersonCell – bail out!
+            fatalError("Unable to dequeue PersonCell.")
+        }
+
+        // if we're still here it means we got a PersonCell, so we can return it
+        return cell
+    }
+
+
+   
 
 }
---
+++ Story/Story/Topic/Controller/topicCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  topicCollectionViewCell.swift
+//  Story
+//
+//  Created by Aries Dwi Prasetiyo on 12/06/20.
+//  Copyright © 2020 Aries Dwi Prasetiyo. All rights reserved.
+//
+
+import UIKit
+
+class topicCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var nameLabel: UILabel!
+}
